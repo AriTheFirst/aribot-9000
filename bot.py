97557182b06764ef0b59d390bff5e49a750865c3
arithefirst <ari@arithefirst.com>
2024-05-31 22:48:30 -0400

    Updated for interactions.py 5.12.1

--- bot.py
+++ bot.py
@@ -11,7 +11,6 @@ import random
 import pymongo
 from dotenv import load_dotenv
 import math
-import urllib.parse
 
 # Grab bot token from enviroment
 load_dotenv()
@@ -67,19 +66,18 @@ There could be multiple reasons for this:
 # ----------------- #
 
 # API Request Command
-@bot.command(
+@interactions.slash_command(
     name="api",
     description="Get data on a user from Discord's API",
-    options = [
-        interactions.Option(
-            name="user",
-            description="The User to get the data of",
-            type=interactions.OptionType.STRING,
-            required=True,
-        ),
-    ],
+    scopes=command_scopes,
 )
-async def api(ctx: interactions.CommandContext, user: str):
+@interactions.slash_option(
+    name="user",
+    description="User to get the data of",
+    required=True,
+    opt_type=interactions.OptionType.STRING
+)
+async def api(ctx: interactions.SlashContext, user: str):
     userchecked = re.sub("[^0-9]", "", f"{user}")
     if api_request(userchecked,False) == "API Request Failure":
         await ctx.send(api_fail_msg,ephemeral=True)
@@ -93,19 +91,18 @@ async def api(ctx: interactions.CommandContext, user: str):
         await ctx.send(embeds=[embed])
 
 # Avatar Command
-@bot.command(
+@interactions.slash_command(
     name="avatar",
     description="Grab a user's avatar",
-    options=[
-        interactions.Option(
-            name="user",
-            description="The User to get the avatar of",
-            type=interactions.OptionType.STRING,
-            required=True,
-        ),
-    ],
+    scopes=command_scopes,
 )
-async def avatar(ctx: interactions.CommandContext, user: str):
+@interactions.slash_option(
+    name="user",
+    description="User to get the avatar of",
+    required=True,
+    opt_type=interactions.OptionType.STRING
+)
+async def avatar(ctx: interactions.SlashContext, user: str):
     userchecked = re.sub("[^0-9]", "", f"{user}")
     if api_request(userchecked,True) == "API Request Failure":
         await ctx.send(api_fail_msg,ephemeral=True)
@@ -114,25 +111,24 @@ async def avatar(ctx: interactions.CommandContext, user: str):
         print(f"Got Avatar URL for User {userchecked}: {avatar_url}")
         embed = interactions.Embed(
             title=f"{api_request(userchecked,False)}'s avatar",
-            image=interactions.EmbedImageStruct(url=f"{avatar_url}"),
             color=embedcolor("#cba6f7")
         )
+        embed.set_image(avatar_url)
         await ctx.send(embeds=[embed])
 
 # Banner Command
-@bot.command(
+@interactions.slash_command(
     name="banner",
     description="Grab a user's banner, and if they don't have nitro give it's hex code",
-    options=[
-        interactions.Option(
-            name="user",
-            description="The User to get the banner of",
-            type=interactions.OptionType.STRING,
-            required=True,
-        ),
-    ],
+    scopes=command_scopes,
 )
-async def banner(ctx: interactions.CommandContext, user: str):
+@interactions.slash_option(
+    name="user",
+    description="User to get the banner of",
+    required=True,
+    opt_type=interactions.OptionType.STRING
+)
+async def banner(ctx: interactions.SlashContext, user: str):
     userchecked = re.sub("[^0-9]", "", f"{user}")
     if api_request(userchecked,True) == "API Request Failure":
         await ctx.send(api_fail_msg,ephemeral=True)
@@ -152,42 +148,45 @@ async def banner(ctx: interactions.CommandContext, user: str):
                 embed = interactions.Embed(
                     title=f"{api_request(userchecked,False)}'s banner",
                     footer=interactions.EmbedFooter(text=f"{banner_hex}"),
-                    image=interactions.EmbedImageStruct(url=f"https://singlecolorimage.com/get/{hex_no_pound}/600x240"),
                     color=embedcolor("#cba6f7")
                 )
+                embed.set_image(f'https://singlecolorimage.com/get/{hex_no_pound}/600x240')
                 await ctx.send(embeds=[embed])
         else:
             banner_url = f"https://cdn.discordapp.com/banners/{userchecked}/{banner_id}.png?size=4096"
             print(f"Got Banner URL for User {userchecked}: {banner_url}")
             embed = interactions.Embed(
                 title=f"{api_request(userchecked,False)}'s banner",
-                image=interactions.EmbedImageStruct(url=f"{banner_url}"),
                 color=embedcolor("#cba6f7")
             )
+            embed.set_image(banner_url)
             await ctx.send(embeds=[embed])
 
 # Info Command
-@bot.command(
+@interactions.slash_command(
     name="info",
     description="Give information about the bot",
+    scopes=command_scopes,
 )
-async def info(ctx: interactions.CommandContext):
+async def info(ctx: interactions.SlashContext):
     await ctx.send("**## [Aribot 9000](https://github.com/AriTheFirst/aribot-9000): By [arithefirst](https://arithefirst.com)**Written in python using [interactions.py](https://github.com/interactions-py) for Discord API and [MongoDB](https://www.mongodb.com/) for storing data of the currency system")
 
 # Ping Commang
-@bot.command(
+@interactions.slash_command(
     name="ping",
     description="Sends Pong",
+    scopes=command_scopes,
 )
-async def ping(ctx: interactions.CommandContext):
+async def ping(ctx: interactions.SlashContext):
     await ctx.send('Pong!')
 
 # Cat Command
-@bot.command(
+@interactions.slash_command(
     name="cat",
     description="Sends a random cat",
+    scopes=command_scopes,
 )
-async def cat(ctx: interactions.CommandContext):
+async def cat(ctx: interactions.SlashContext):
     # Make Request to the Cat API
     url = "https://api.thecatapi.com/v1/images/search?limit=1&has_breeds=1"
     headers = {"x-api-key": CAT_TOKEN}
@@ -203,35 +202,33 @@ async def cat(ctx: interactions.CommandContext):
         breed = breed_full[0]['name']
         embed = interactions.Embed(
             title=f"Check out this {breed} cat!",
-            image=interactions.EmbedImageStruct(url=f"{cat_url}"),
             color=embedcolor("#cba6f7")
         )
+        embed.add_image(cat_url)
         await ctx.send(embeds=[embed])
     else:
         print(f"API Request Failed with code {response.status_code}")
         await ctx.send(f"Error Communicating with https://thecatapi.com/v1/images/search/ ({response.status_code})")
 
 # Coinflip Command
-@bot.command(
+@interactions.slash_command(
     name="coinflip",
     description="Flips a Coin",
-    scope=command_scopes,
-    options=[
-        interactions.Option(
-            name="bet",
-            description="Heads or Tails",
-            type=interactions.OptionType.STRING,
-            required=False,
-        ),
-        interactions.Option(
-            name="wager",
-            description="Amount of coins to wager",
-            type=interactions.OptionType.INTEGER,
-            required=False,
-        )
-    ]
+    scopes=command_scopes,
 )
-async def coinflip(ctx: interactions.CommandContext, bet: str = None, wager: int = None):
+@interactions.slash_option(
+    name="wager",
+    description="Amount of coins to wager",
+    required=True,
+    opt_type=interactions.OptionType.INTEGER
+)
+@interactions.slash_option(
+    name="bet",
+    description="Heads or Tails",
+    required=True,
+    opt_type=interactions.OptionType.STRING
+)
+async def coinflip(ctx: interactions.SlashContext, bet: str = None, wager: int = None):
     result = random.SystemRandom().randint(0, 1)
     if bet is None or wager is None:
         if result == 0:
@@ -265,20 +262,18 @@ async def coinflip(ctx: interactions.CommandContext, bet: str = None, wager: int
                     await ctx.send(f"You Lose. **{comma_seperate(abs(int(wager)))}** coins have been removed from your account.\nYour new balance is **{comma_seperate(newbalance)}**!")
 
 # Timezones Command
-@bot.command(
+@interactions.slash_command(
     name="timezone",
     description="Send the time in the 4 Major US Timezones, Optionally specify an extra timezone",
-    scope=command_scopes,
-    options=[
-        interactions.Option(
-            name="timezone",
-            description="An optional extra timezone to get the time of (in Continent/City format)",
-            type=interactions.OptionType.STRING,
-            required=False,
-        ),
-    ],
+    scopes=command_scopes,
 )
-async def timezone(ctx: interactions.CommandContext, timezone: str = None):
+@interactions.slash_option(
+    name="timezone",
+    description="An optional extra timezone to get the time of (in Continent/City format)",
+    opt_type=interactions.OptionType.STRING,
+    required=False,
+)
+async def timezone(ctx: interactions.SlashContext, timezone: str = None):
     if timezone is None:
         time_zones = [
             "America/New_York",
@@ -301,29 +296,27 @@ async def timezone(ctx: interactions.CommandContext, timezone: str = None):
             await ctx.send("Invalid timezone provided.")
 
 # Identify Command
-@bot.command(
+@interactions.slash_command(
     name="identify",
     description="Gives you your user id and this server's id",
-    scope=command_scopes,
+    scopes=command_scopes,
 )
-async def identify(ctx: interactions.CommandContext):
+async def identify(ctx: interactions.SlashContext):
     await ctx.send(f'**User ID:** {ctx.user.id}\n**Guild ID:** {ctx.guild_id}')
 
 # Checkbalance Command
-@bot.command(
+@interactions.slash_command(
     name="checkbalance",
     description="Checks your balance",
-    scope=command_scopes,
-    options=[
-        interactions.Option(
-            name="user",
-            description="user to check the balance of (optional)",
-            type=interactions.OptionType.STRING,
-            required=False
-        ),
-    ]
+    scopes=command_scopes,
 )
-async def checkbal(ctx: interactions.CommandContext, user: str = None):
+@interactions.slash_option(
+    name="user",
+    description="user to check the balance of",
+    required=False,
+    opt_type=interactions.OptionType.STRING
+)
+async def checkbal(ctx: interactions.SlashContext, user: str = None):
     if user == None:
             query = {"name": str(ctx.user.id)}
             usercol = database[f"server-{ctx.guild_id}"]
@@ -350,28 +343,25 @@ async def checkbal(ctx: interactions.CommandContext, user: str = None):
             formatted_blance = comma_seperate(balance)
             await ctx.send(f"<@{userchecked}>'s balance is **{formatted_blance}** coins.")
 
-
 # Setbalance Command
-@bot.command(
+@interactions.slash_command(
     name="setbalance",
     description="Sets a user's balance",
-    scope=command_scopes,
-    options=[
-        interactions.Option(
-            name="user",
-            description="User to modify the balance of",
-            type=interactions.OptionType.STRING,
-            required=True,
-        ),
-        interactions.Option(
-            name="amt",
-            description="New Balance for the user",
-            type=interactions.OptionType.INTEGER,
-            required=True,
-        ),
-    ]
+    scopes=command_scopes,
 )
-async def setbalance(ctx: interactions.CommandContext, user: str, amt: int):
+@interactions.slash_option(
+    name="user",
+    description="User to modify the balance of",
+    required=False,
+    opt_type=interactions.OptionType.STRING
+)
+@interactions.slash_option(
+    name="amt",
+    description="New Balance for the user",
+    required=False,
+    opt_type=interactions.OptionType.INTEGER
+)
+async def setbalance(ctx: interactions.SlashContext, user: str, amt: int):
     if ctx.user.id == 613358761901424652:
         userchecked = re.sub("[^0-9]", "", f"{user}")
         query = {"name": f"{userchecked}"}
@@ -384,12 +374,12 @@ async def setbalance(ctx: interactions.CommandContext, user: str, amt: int):
         await ctx.send("This command can only be run by <@613358761901424652> herself.")
 
 # Fish Command
-@bot.command(
+@interactions.slash_command(
     name="fish",
     description="Go fishing",
-    scope=command_scopes,
+    scopes=command_scopes,
     )
-async def fish(ctx: interactions.CommandContext):
+async def fish(ctx: interactions.SlashContext):
     items = ['Old Boot', 'Rock', 'Wallet', 'Oar Fish', 'Funny Stupid Fish', 'Salmon', 'Sea Bunny', 'Wedding Ring', 'Mortimer: The Ancient Evil Goblin That Steals your Coins']
     probabilities = [20, 20, 15, 15, 10, 10, 5, 2.5, 2.5,]
     total_percentage = sum(probabilities)
@@ -516,26 +506,24 @@ async def fish(ctx: interactions.CommandContext):
                     await ctx.send(f'You caught the **{' '.join(fished_fish)}**!\nHe took half your coins and now you have **{math.floor(int(balance)/2)}**')
 
 # Send Command
-@bot.command(
+@interactions.slash_command(
     name="send",
     description="Sends money to another user",
-    scope=command_scopes,
-    options=[
-        interactions.Option(
-            name="user",
-            description="User to send money to",
-            type=interactions.OptionType.STRING,
-            required=True,
-        ),
-        interactions.Option(
-            name="amt",
-            description="Amount of money to send",
-            type=interactions.OptionType.INTEGER,
-            required=True,
-        ),
-    ]
+    scopes=command_scopes,
 )
-async def send(ctx: interactions.CommandContext, user: str = None, amt: int = None):
+@interactions.slash_option(
+    name="user",
+    description="User to send money to",
+    required=True,
+    opt_type=interactions.OptionType.STRING
+)
+@interactions.slash_option(
+    name="amt",
+    description="Amount of money to send",
+    required=True,
+    opt_type=interactions.OptionType.INTEGER
+)
+async def send(ctx: interactions.SlashContext, user: str = None, amt: int = None):
     userchecked = re.sub("[^0-9]", "", f"{user}")
     query_reciver = {"name": str(userchecked)}
     query_sender = {"name": str(ctx.user.id)}
@@ -568,57 +556,13 @@ async def send(ctx: interactions.CommandContext, user: str = None, amt: int = No
             )
             await ctx.send(embeds=[embed])
 
-@bot.command(
-    name="spotify",
-    description="Searches Spotify through the Spotify API",
-    scope=command_scopes,
-    options=[
-        interactions.Option(
-            name="query",
-            description="Search Query",
-            type=interactions.OptionType.STRING,
-            required=True,
-        ),
-    ]
-)
-async def spotify(ctx: interactions.CommandContext, query: str = None):
-    keygen_url = "https://accounts.spotify.com/api/token"
-    keygen_headers = {"Content-Type": "application/x-www-form-urlencoded",}
-    keygen_data = {
-    "grant_type": "client_credentials",
-    "client_id": "80b7971a22ec46358240b5bde22180a4",
-    "client_secret": f"{SPOTIFY_CLIENT_SECRET}",
-    }
-
-    keygen_request = requests.post(keygen_url, headers=keygen_headers, data=keygen_data)
-    spotify_api_key = keygen_request.json()["access_token"]
-
-    url = f"https://api.spotify.com/v1/search?q={query}&type=track&limit=1"
-    headers = {'Authorization': f'Bearer {spotify_api_key}',}
-    request = requests.get(url, headers=headers)
-    reply = request.json()
-    
-    embed = interactions.Embed(
-        title=f"Search Result ",
-        description=f'<@{ctx.user.id}> searched for "{query}"',
-        color=embedcolor("#cba6f7"),
-        thumbnail=interactions.EmbedImageStruct(url=reply['tracks']['items'][0]['album']['images'][0]['url']),
-        fields=[
-            interactions.EmbedField(name="Title", value=reply['tracks']['items'][0]['name'], inline=False),
-            interactions.EmbedField(name=f"Album Title", value=reply['tracks']['items'][0]['album']['name'], inline=False),
-            interactions.EmbedField(name=f"Album Artist", value=reply['tracks']['items'][0]['album']['artists'][0]['name'], inline=False),
-            interactions.EmbedField(name=f"Track URL", value=reply['tracks']['items'][0]['external_urls']['spotify'], inline=False),
-        ]
-    )
-    await ctx.send(embeds=[embed])
-
 # Leaderboard Command
-@bot.command(
+@interactions.slash_command(
     name="leaderboard",
     description="Display server leaderboard",
-    scope=command_scopes,
+    scopes=command_scopes,
     )
-async def Leaderboard(ctx: interactions.CommandContext):
+async def Leaderboard(ctx: interactions.SlashContext):
     query = {"name": ""}
     usercol = database[f"server-{ctx.guild_id}"]
     cursor = usercol.find({}, {"name": 1, "amt": 1, "_id": 0})
@@ -633,20 +577,12 @@ async def Leaderboard(ctx: interactions.CommandContext):
     await ctx.send(embeds=[embed])
 
 # Help Command
-@bot.command(
+@interactions.slash_command(
     name = "help",
     description = "Gives command help",
-    scope=command_scopes,
-    options=[
-        interactions.Option(
-            name="command",
-            description="Command to get help with",
-            required="False",
-            type=interactions.OptionType.STRING
-        )
-    ]
+    scopes=command_scopes,
 )
-async def help (ctx: interactions.CommandContext,command: str = None ):
+async def help (ctx: interactions.SlashContext):
     embed = interactions.Embed(
         title="Commands List",
         color=embedcolor("#cba6f7"),
@@ -666,3 +602,4 @@ print("""   ___       _ ___       __    ___  ___  ___  ___
 https://arithefirst.com
 """)
 bot.start()
+
